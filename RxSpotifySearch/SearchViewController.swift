568875278e7282ae4ec7430181c726eeb19fd66b
Adam Borek <aborek@allinmobile.co>
2016-11-04 21:44:36 +0100

    Start here

---
+++ RxSpotifySearch/SearchViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  RxSpotifySearch
+//
+//  Created by Adam Borek on 04/11/2016.
+//  Copyright (c) 2016 Adam Borek. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import SwiftyJSON
+import RxSwift
+import RxCocoa
+
+class SearchViewController: UITableViewController {
+    fileprivate enum Constatnts {
+        static let minimumSearchLength = 3
+    }
+    @IBOutlet weak var searchBar: UISearchBar!
+    private let disposeBag = DisposeBag()
+    
+    let spotifyClient = SpotifyClient()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.titleView = searchBar
+        setupTableView()
+    }
+
+    private func setupTableView() {
+        tableView.delegate = nil
+        tableView.rx.setDelegate(self)
+                .addDisposableTo(disposeBag)
+        tableView.dataSource = nil
+    }
+}
